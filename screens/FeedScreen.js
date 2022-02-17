d58950185da5f283d0630d6990eae05e323ed6da
Myriam Mira <77079498+myracodes@users.noreply.github.com>
2022-02-17 11:03:07 +0100

    Finish last quest

--- screens/FeedScreen.js
+++ screens/FeedScreen.js
@@ -1,8 +1,44 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Image, FlatList, StyleSheet } from "react-native";
 
 export function FeedScreen() {
-
- return <Text>This is FeedScreen</Text>
-
+  const [serverImagesUrls, setServerImagesUrls] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const filesUrl = await axios.get(
+        "https://wildstagram.nausicaa.wilders.dev/list"
+      );
+      console.log("filesurls", filesUrl.data);
+      setServerImagesUrls(filesUrl.data);
+    })();
+  }, []);
+  return serverImagesUrls.length > 0 ? (
+    <FlatList
+      data={serverImagesUrls}
+      keyExtractor={(serverImageURI) => serverImageURI}
+      renderItem={(itemData) => {
+        console.log("item", itemData);
+        return (
+          <>
+            <Image
+              style={styles.image}
+              source={{
+                uri:
+                  "https://wildstagram.nausicaa.wilders.dev/files/" +
+                  itemData.item,
+              }}
+            />
+          </>
+        );
+      }}
+    />
+  ) : null;
 }
+
+const styles = StyleSheet.create({
+  image: {
+    resizeMode: "contain",
+    height: 500,
+  },
+});
